c9bda258012646bfe1e00dc1038631c4d4e5baa8
VicolMihai <mihai.vicol@3pillarglobal.com>
2022-11-13 11:13:33 +0200

    added docstrings

--- flatten_lists.py
+++ flatten_lists.py
@@ -2,6 +2,12 @@ import unittest
 
 
 def flatten(list_to_flatten, depth):
+    """
+    flatten list to until given depth or maximum possible
+    :param list_to_flatten: list
+    :param depth: int
+    :return: flattened list
+    """
     if depth == 0:
         return list_to_flatten
     aux_list = []
@@ -14,6 +20,13 @@ def flatten(list_to_flatten, depth):
 
 
 def make_flatten(list_a, list_b, depth):
+    """
+    flatten two list with flatten function
+    :param list_a: list
+    :param list_b: list
+    :param depth: int
+    :return: tuple with list flattened
+    """
     return flatten(list_a, depth), flatten(list_b, depth)
 
 
@@ -25,3 +38,7 @@ class FlattenTest (unittest.TestCase):
     def test_depth_lower(self):
         r = make_flatten([[[[[[1, 2, 3]]], 3, 4], 5]], [[[[4, 5, 6]], 7]], 4)
         self.assertEqual(([[1, 2, 3], 3, 4, 5], [4, 5, 6, 7]), r, "i'm testing two lists")
+
+    def test_empty_list(self):
+        res = make_flatten([], [], 20)
+        self.assertEqual(res, ([], []))
--- sort_dictionaries.py
+++ sort_dictionaries.py
@@ -2,19 +2,33 @@ import unittest
 
 
 def read_from_file():
+    """
+    read a file that contains a list of dictionaries
+    keys and values are separated by a whitespace
+    dictionaries are separated by new line
+    :return: list of dictionaries
+    """
     file = open("dictionaries", "r")
     list_dict_file = []
+
     for line in file.readlines():
         words_list = line.split()
         a = {}
         for words in range(0, len(words_list)-1, 2):
             a[words_list[words]] = words_list[words+1]
         list_dict_file.append(a)
+
     file.close()
     return list_dict_file
 
 
 def write(list_dict):
+    """
+    write in file indexes of sorted dictionaries
+    read again from file to see the indexes of dictionaries
+    :param list_dict: list of dictionaries
+    :return: None
+    """
     f = open('sorted_dictionaries', 'w')
     list_original = read_from_file()
     for i in list_dict:
@@ -23,11 +37,21 @@ def write(list_dict):
 
 
 def cmp_func(b):
+    """
+    compare key, it compares the values of the lowest alphabetical key
+    :param b: dictionary
+    :return: value of lowest alphabetical key
+    """
     sat = sorted(b)
     return int(b[sat[0]])
 
 
 def sort(list_to_sort):
+    """
+    sort list of dictionaries
+    :param list_to_sort: list of dictionaries
+    :return: sorted list
+    """
     list_to_sort.sort(key=cmp_func)
     return list_to_sort
 
@@ -42,6 +66,11 @@ class SortTest(unittest.TestCase):
         res = sort(list_ex)
         self.assertEqual(res, [{'ag': '8', 'bd': '23', 'df': '34'}, {'ah': '9', 'bf': '23', 'jd': '34'}, {'aa': '12', 'ba': '33', 'cb': '45'}, {'ac': '11', 'ab': '23', 'cb': '34'}])
 
-
+    def test_read_file(self):
+        res = read_from_file()
+        self.assertEqual(res, [{'ac': '11', 'ab': '23', 'cb': '34'},
+                               {'ah': '9', 'bf': '23', 'jd': '34'},
+                               {'ag': '8', 'bd': '23', 'df': '34'},
+                               {'aa': '12', 'ba': '33', 'cb': '45'}])
 
 
