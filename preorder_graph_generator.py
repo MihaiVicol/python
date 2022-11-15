4d7f799435d514c6a4cb6f951d1bfd48f93146fe
VicolMihai <mihai.vicol@3pillarglobal.com>
2022-11-15 11:07:16 +0200

    implement tests

--- preorder_graph_generator.py
+++ preorder_graph_generator.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def preorder_generator(graph_tuple):
     for i in graph_tuple:
@@ -7,6 +9,11 @@ def preorder_generator(graph_tuple):
             yield i
 
 
-graph_t = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
-for node in preorder_generator(graph_t):
-    print(node)
+class TestGenerator(unittest.TestCase):
+    def test_preorder(self):
+        graph_t = ('b', ('a', None, None), ('z', ('c', None, None), ('zz', None, None)))
+        res = []
+        for node in preorder_generator(graph_t):
+            res.append(node)
+        self.assertEqual(res, ['b', 'a', 'z', 'c', 'zz'])
+
